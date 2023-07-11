18beb3b86648a8726570421d3f7c23ae3fc5bf6b
Jaime Pineda <jpin730@pm.me>
2023-07-11 16:49:55 -0600

    refactor: :recycle: add global document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
+// TODO: no title in document head
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Next Pokemon</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
