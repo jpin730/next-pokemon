885443b2cfe782bc45fdac78af848a684fb63056
Jaime Pineda <jpin730@pm.me>
2023-07-11 17:44:24 -0600

    feat: :package: add next ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,40 @@
 // TODO: no title in document head
 /* eslint-disable @next/next/no-title-in-document-head */
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+import React from "react";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <title>Next Pokemon</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {CssBaseline.flush()}
+          <title>Next Pokemon</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
