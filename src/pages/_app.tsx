129bf2517ee907bf52acb9aa6db76bca4409e8f4
Jaime Pineda <jpin730@pm.me>
2023-07-25 12:17:34 -0600

    feat: :lipstick: add dark theme to next ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 
+import { darkTheme } from "@/themes";
+
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
