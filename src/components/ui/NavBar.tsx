eac045180b51416706a5989d635ad8f3a59e8f0a
Jaime Pineda <jpin730@pm.me>
2023-08-01 19:55:17 -0600

    feat: :lipstick: refactor nav bar component

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,17 +1,39 @@
-import { Row, Text } from "@nextui-org/react";
+import { Button, Navbar, Row, Text } from "@nextui-org/react";
 import Link from "next/link";
 import { FC } from "react";
 
+import { HeartIcon } from "../icons";
+
 export const NavBar: FC = () => {
   return (
-    <div>
-      <Row className="container" justify="space-between">
-        <Link href="/">
-          <Text h2 weight="light">
-            Next Pokemon
-          </Text>
-        </Link>
+    <Navbar variant="sticky" css={{ width: "100%" }}>
+      <Row
+        className="container"
+        justify="space-between"
+        align="center"
+        wrap="wrap"
+      >
+        <Navbar.Brand>
+          <Link href="/" style={{ display: "flex", gap: "0.5rem" }}>
+            <Text h2 weight="light" className="hoverable" css={{ margin: 0 }}>
+              Next Pokemon
+            </Text>
+          </Link>
+        </Navbar.Brand>
+
+        <Navbar.Content>
+          <Button
+            auto
+            ghost
+            as={Link}
+            color="error"
+            href="/favorites"
+            icon={<HeartIcon filled />}
+          >
+            <Text color="white">Favorites</Text>
+          </Button>
+        </Navbar.Content>
       </Row>
-    </div>
+    </Navbar>
   );
 };
